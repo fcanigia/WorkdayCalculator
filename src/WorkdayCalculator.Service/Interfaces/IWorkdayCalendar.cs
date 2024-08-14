945571d3b032f1324cf46c6e7b9ee4a82da1f03f
Facundo Canigia <facundo.canigia@gmail.com>
2024-08-14 17:36:12 -0300

    create interface

---
+++ src/WorkdayCalculator.Service/Interfaces/IWorkdayCalendar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkdayCalculator.Service.Interfaces;
+
+public interface IWorkdayCalendar
+{
+    void SetHoliday(DateTime date);
+    void SetRecurringHoliday(int month, int day);
+    void SetWorkdayStartAndStop(int startHours, int startMinutes,
+    int stopHours, int stopMinutes);
+    DateTime GetWorkdayIncrement(DateTime startDate, decimal
+    incrementInWorkdays);
+}
