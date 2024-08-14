7a4f5f95f698ad4541ef967d656066c954dd2b0b
Facundo Canigia <facundo.canigia@gmail.com>
2024-08-14 17:40:37 -0300

    create WorkdayCalendar class and implement interface

---
+++ src/WorkdayCalculator.Service/Services/WorkdayCalendar.cs
@@ -0,0 +1,26 @@
+﻿using WorkdayCalculator.Service.Interfaces;
+
+namespace WorkdayCalculator.Service.Services;
+
+public class WorkdayCalendar : IWorkdayCalendar
+{
+    public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetHoliday(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetRecurringHoliday(int month, int day)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
+    {
+        throw new NotImplementedException();
+    }
+}
