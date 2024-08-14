9a6de6f9e0c113f5bb128ab1ef248aa5cf8134eb
Facundo Canigia <facundo.canigia@gmail.com>
2024-08-14 17:42:37 -0300

    adding readme and git ignore to proyect

--- src/WorkdayCalculator.Service/Interfaces/IWorkdayCalendar.cs
+++ src/WorkdayCalculator.Service/Interfaces/IWorkdayCalendar.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorkdayCalculator.Service.Interfaces;
+﻿namespace WorkdayCalculator.Service.Interfaces;
 
 public interface IWorkdayCalendar
 {
