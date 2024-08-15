33f4eadf63d12d725883963b361a1124291b3c79
Facundo Canigia <facundo.canigia@gmail.com>
2024-08-14 21:44:26 -0300

    fill program cs with calculator call

--- src/WorkdayCalculator.Service/Program.cs
+++ src/WorkdayCalculator.Service/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace WorkdayCalculator;
+﻿using WorkdayCalculator.Service.Services;
+
+namespace WorkdayCalculator.Service;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var calendar = new WorkdayCalendar();
+
+        calendar.SetWorkdayStartAndStop(8, 0, 16, 0);
+        calendar.SetRecurringHoliday(5, 17);
+        calendar.SetHoliday(new DateTime(2004, 5, 27));
+
+        string format = "dd-MM-yyyy HH:mm";
+
+        var start = new DateTime(2004, 5, 24, 18, 5, 0);
+        decimal increment = -5.5m;
+
+        var incrementedDate = calendar.GetWorkdayIncrement(start, increment);
+
+        Console.WriteLine(start.ToString(format) + " with an addition of " + increment +
+                            " work days is " + incrementedDate.ToString(format));
     }
 }
